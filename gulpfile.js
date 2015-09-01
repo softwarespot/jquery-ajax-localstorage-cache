e9a4a69b7847535811088baf60d1aa38d8750e74
softwarespot <softwarespotapps@gmail.com>
2015-09-01 20:36:50 +0300

    Added assets object

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,27 @@ var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var del = require('del');
 
+// Assets for the project
+var Assets = {
+    main: 'jquery-ajax-localstorage-cache.js',
+    minified: 'jquery-ajax-localstorage-cache.min.js'
+};
+
 // Clean the current directory
 gulp.task('clean', function (cb) {
-    del(['jquery-ajax-localstorage-cache.min.js'], cb);
+    del([Assets.minified], cb);
 });
 
 // Check the code meets the following standards outlined in .jshintrc
 gulp.task('jshint', function () {
-    return gulp.src('./jquery-ajax-localstorage-cache.js')
+    return gulp.src('./' + Assets.main)
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 // Uglify aka minify the main file
 gulp.task('uglify', ['clean'], function () {
-    return gulp.src('./jquery-ajax-localstorage-cache.js')
+    return gulp.src('./' + Assets.main)
         .pipe(uglify({
             // See the uglify documentation for more details
             compress: {
@@ -38,7 +44,7 @@ gulp.task('uglify', ['clean'], function () {
 
 // Watch for changes to the main file to trigger hinting and minification
 gulp.task('watch', function () {
-    gulp.watch('./jquery-ajax-localstorage-cache.js', ['jshint', 'uglify']);
+    gulp.watch('./' + Assets.main, ['jshint', 'uglify']);
 });
 
 // Register the default task to hinting and minification
