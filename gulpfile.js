0ba4fc7363870cb2127510f8d6dd629c0093629d
softwarespot <softwarespotapps@gmail.com>
2015-11-30 21:37:52 +0200

    Updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,15 @@ var jshint = require('gulp-jshint');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
+// Assets for the project
+var Assets = {
+    main: './jquery-ajax-localstorage-cache.js',
+    minified: './jquery-ajax-localstorage-cache.min.js',
+    source: './',
+};
+
 // See the uglify documentation for more details
-var uglifySettings = {
+var _uglifySettings = {
     compress: {
         comparisons: true,
         conditionals: true,
@@ -19,30 +26,24 @@ var uglifySettings = {
     },
 };
 
-// Assets for the project
-var Assets = {
-    main: 'jquery-ajax-localstorage-cache.js',
-    minified: 'jquery-ajax-localstorage-cache.min.js',
-};
-
 // Check the code meets the following standards outlined in .jshintrc
-gulp.task('jshint', function() {
-    return gulp.src('./' + Assets.main)
+gulp.task('jshint', function jsHintTask() {
+    return gulp.src(Assets.main)
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
 // Uglify aka minify the main file
-gulp.task('uglify', function() {
-    return gulp.src('./' + Assets.main)
-        .pipe(uglify(uglifySettings))
+gulp.task('uglify', function uglifyTask() {
+    return gulp.src(Assets.main)
+        .pipe(uglify(_uglifySettings))
         .pipe(rename(Assets.minified))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest(Assets.source));
 });
 
 // Watch for changes to the main file
-gulp.task('watch', function() {
-    gulp.watch('./' + Assets.main, ['jshint', 'uglify']);
+gulp.task('watch', function watchTask() {
+    gulp.watch(Assets.main, ['jshint', 'uglify']);
 });
 
 // Register the default task
